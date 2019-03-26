5e6bfb0821aa1ada7cd456862155df27a25f63e8
Timur Z <timurzil@gmail.com>
2019-03-26 22:40:05 +0100

    parenthesis cut-cut clear

--- parenthesis.js
+++ parenthesis.js
@@ -1,19 +1,17 @@
 let input_string_good = "55ddrt(test example(11(1)) n1)";
-let input_string_bad_1 = "123456(test ex()(am(ple)(11()1)) n1)";
-let input_string_bad_2 = "1123(test ex )( )a(m(ple11(1)) n1)";
+let input_string_bad_1 = "123456(test ex()(am(ple(11()1)) n1)";
+let input_string_good_2 = "1123(test ex )( )a(m(ple11(1)) n1)";
 let input_string_bad_3 = "12)(test ex )( ample(11(1) n1)";
 
 
 console.log(input_string_good, is_there_enough_closing(input_string_good));
 console.log(input_string_bad_1, is_there_enough_closing(input_string_bad_1));
-console.log(input_string_bad_2, is_there_enough_closing(input_string_bad_2));
+console.log(input_string_good_2, is_there_enough_closing(input_string_good_2));
 console.log(input_string_bad_3, is_there_enough_closing(input_string_bad_3));
 
 function char_cut(string_to_cut, num_char)
 {
-
-return string_to_cut.slice(0,num_char) + string_to_cut.slice(num_char+1);
-
+    return string_to_cut.slice(0,num_char) + string_to_cut.slice(num_char+1);
 }
 
 function is_there_enough_closing (string_with_closes)
@@ -25,9 +23,7 @@ function is_there_enough_closing (string_with_closes)
         // console.log(s_begin,s_end);
         if (parenthesis_open_1 == -1 && parenthesis_close_1 == -1)
     {
-        console.log("Exit 1");
-        return " Correct";
-    
+                return " Correct";
     }
     
     else if ((parenthesis_open_1 == -1) || (parenthesis_close_1 == -1)
@@ -35,14 +31,11 @@ function is_there_enough_closing (string_with_closes)
                 return " Incorrect";
         else
         {
-            
             sub_string = char_cut(string_with_closes,parenthesis_open_1);
             parenthesis_close_1 = sub_string.indexOf(")");
             sub_string = char_cut(sub_string,parenthesis_close_1);
     
-            console.log(sub_string);
+            // console.log(sub_string);
             return is_there_enough_closing(sub_string);               
         }
-    
- 
 }
